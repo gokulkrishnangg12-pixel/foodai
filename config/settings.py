b817a0e0bfcd2d4f9b05cc7d6fdac0e4dd823b58
GOKUL KRISHNA <gokulkrishnangg12@gmail.com>
2026-03-25 23:01:22 +0530

    Fix MIDDLEWARE bug, add whitenoise to requirements and comment out staticfiles manifest

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,13 +130,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-# Add to foodhealth/settings.py
-MIDDLEWARE = [
-    # ... other middleware
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-]
-
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
